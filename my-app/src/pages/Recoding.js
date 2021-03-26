cc3c882af086d9c710c65b5f0352be361c7c6552
BlockiyDmitriy <51110104+BlockiyDmitriy@users.noreply.github.com>
2021-03-26 10:47:45 +0300

    Page Recoding

---
+++ my-app/src/pages/Recoding.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Layout } from '../layout';
+
+const Recoding = (props) => (
+    <Layout>
+        { props.name}
+    </Layout>
+);
+
+export default Recoding
